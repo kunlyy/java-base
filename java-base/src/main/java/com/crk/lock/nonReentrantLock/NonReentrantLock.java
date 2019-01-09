9bb3675da6ca155ab4606fe4962d88165015cbbb
chenrongkun <chenrongkun@sunlands.com>
2019-01-09 11:30:29 +0800

    first commit

---
+++ java-base/src/main/java/com/crk/lock/nonReentrantLock/NonReentrantLock.java
@@ -0,0 +1,22 @@
+package com.crk.lock.nonReentrantLock;
+
+/**
+ * Created by chenrongkun on 2018/5/17.
+ * 不可重入锁
+ */
+public class NonReentrantLock {
+	private boolean lock = false;
+
+	public synchronized void lock() throws InterruptedException {
+		while (lock) {
+			wait();
+		}
+		lock = true;
+	}
+
+	public synchronized void unLock() {
+		lock = false;
+		notifyAll();
+	}
+
+}
